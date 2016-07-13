e088829c0c7531b804b2761a6f97b2d6145ed5a3
andygulin <andygulin@hotmail.com>
2016-07-13 15:53:10 +0800

    ...

---
+++ json_test.py
@@ -0,0 +1,18 @@
+import json
+import time
+
+user = {
+        "id" : 1,
+        "name" : "andy",
+        "age" : 11,
+        "address" : "shanghai",
+        "marry" : True,
+        "hobby" : ["music", "book", "TV"],
+        "createdAt" : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+}
+
+json_str = json.dumps(user)
+print(json_str)
+
+user = json.loads(json_str)
+print(user)
