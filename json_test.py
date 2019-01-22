9842697de479a5d4e9264b4f04c2b8233ab83150
gulin <andygulin@hotmail.com>
2019-01-22 15:26:18 +0800

    123

--- json_test.py
+++ json_test.py
@@ -6,13 +6,37 @@ user = {
     "name": "andy",
     "age": 11,
     "address": "shanghai",
-    "marry": True,
-    "hobby": ["music", "book", "TV"],
+    "married": True,
+    "hobbies": ["music", "book", "TV"],
     "createdAt": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 }
 
 json_str = json.dumps(user)
 print(json_str)
-
 user = json.loads(json_str)
 print(user)
+
+users = [
+    {
+        "id": 1,
+        "name": "小明",
+        "age": 11,
+        "address": "上海",
+        "married": True,
+        "hobbies": ["music", "book", "TV"],
+        "createdAt": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    },
+    {
+        "id": 2,
+        "name": "小红",
+        "age": 12,
+        "address": "北京",
+        "married": False,
+        "hobbies": ["play", "football", "TV"],
+        "createdAt": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    }
+]
+json_str = json.dumps(users)
+print(json_str)
+users = json.loads(json_str)
+print(users)
